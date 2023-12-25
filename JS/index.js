5c18aa62bf05f9fabb7cb30648b9e123fc713171
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2023-12-25 13:56:31 +0300

    Implementing a Sticky Navigation Bar

--- JS/index.js
+++ JS/index.js
@@ -33,6 +33,30 @@ allLinks.forEach((el) => {
   });
 });
 
+//Sticky Navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+
+obs.observe(sectionHeroEl);
+
 function checkFlexGap() {
   var flex = document.createElement("div");
   flex.style.display = "flex";
